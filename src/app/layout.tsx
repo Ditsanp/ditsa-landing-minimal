66cc361013abfcff3636ba451d1b380c3f8f55b1
tyzrex <sulavbaral58@gmail.com>
2024-09-14 19:25:27 +0545

    add og image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import LenisWrapper from "@/components/wrappers/lenis-wrapper";
 import { Bebas_Neue, Nunito_Sans } from "next/font/google";
 import Footer from "@/components/globals/footer";
 import { Toaster } from "sonner";
+
 const averta = Bebas_Neue({
   weight: ["400"],
   variable: "--font-averta",
@@ -38,6 +39,22 @@ export const metadata: Metadata = {
     "Digital Transformation Agency",
     "Digital Transformation Company",
   ],
+  openGraph: {
+    title:
+      "DITSA: Your Gateway for Digital Transformation with Innovative Solutions and Advanced Technology",
+    description:
+      "DITSA: Your Gateway for Digital Transformation with Innovative Solutions and Advanced Technology",
+    url: "https://ditsanp.com/",
+    siteName:
+      "DITSA: Your Gateway for Digital Transformation with Innovative Solutions and Advanced Technology",
+    images: [
+      {
+        url: "/ditsaog.webp",
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
