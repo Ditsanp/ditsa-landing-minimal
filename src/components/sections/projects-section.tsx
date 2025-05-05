4079efc6a78c207415e5f2fb263974f708968283
tyzrex <sulavbaral58@gmail.com>
2025-05-05 22:03:01 +0545

    image fix v3

--- src/components/sections/projects-section.tsx
+++ src/components/sections/projects-section.tsx
@@ -1,23 +1,3 @@
-import Web1 from "/images/services/web/web.png";
-import Web2 from "/images/services/web/web1.png";
-import Web3 from "/images/services/web/web3.png";
-
-import App1 from "/images/services/app/app1.png";
-import App2 from "/images/services/app/app2.jpg";
-import App3 from "/images/services/app/app3.png";
-
-import Data1 from "/images/services/data/data1.png";
-import Data2 from "/images/services/data/data2.jpg";
-import Data3 from "/images/services/data/data3.png";
-
-import DM1 from "/images/services/digital-marketing/dm1.jpg";
-import DM2 from "/images/services/digital-marketing/dm2.png";
-import DM3 from "/images/services/digital-marketing/dm3.png";
-
-import GD1 from "/images/services/graphic/gd1.png";
-import GD2 from "/images/services/graphic/gd3.png";
-import GD3 from "/images/services/graphic/gd4.png";
-
 import ProjectCard from "../reusables/project-card";
 import SectionHeader from "../reusables/section-header";
 import SectionWrapper from "../wrappers/section-wrapper";
@@ -46,7 +26,7 @@ const projects = [
   {
     name: "Web Design and Development",
     description:
-      "Our web development services focus on creating websites that are not only visually stunning but also highly functional. We offer custom website design tailored to reflect your brand identity, responsive design to ensure your website looks great and performs well on all devices, and e-commerce solutions for building secure and scalable online stores. Additionally, we develop websites on content management systems (CMS) like WordPress, Joomla, and Drupal, and provide regular updates and maintenance to keep your site secure and up-to-date. Our goal is to create user-friendly websites that deliver an exceptional user experience and drive business growth.",
+      "Our web development services focus on creating websites that are not only visually stunning but also highly functional...",
     techs: [
       "web development",
       "responsive design",
@@ -57,12 +37,16 @@ const projects = [
       "cross-device compatibility",
       "user-friendly websites",
     ],
-    images: [Web1, Web2, Web3],
+    images: [
+      "/images/services/web/web.png",
+      "/images/services/web/web1.png",
+      "/images/services/web/web3.png",
+    ],
   },
   {
     name: "Digital Marketing",
     description:
-      "Our digital marketing services are aimed at driving traffic, increasing engagement, and boosting conversions. We employ a multi-faceted approach to ensure your brand gains the visibility it deserves. Our strategies include search engine optimization (SEO) to improve your website's ranking on search engines, pay-per-click (PPC) advertising for immediate traffic and conversions, and social media marketing to build and engage your audience on platforms like Facebook, Instagram, Twitter, and LinkedIn. Additionally, we offer content marketing to create valuable content that attracts, engages, and converts your target audience, as well as email marketing to develop and manage campaigns that nurture leads and drive sales.",
+      "Our digital marketing services are aimed at driving traffic, increasing engagement, and boosting conversions...",
     techs: [
       "digital marketing",
       "SEO",
@@ -74,13 +58,16 @@ const projects = [
       "audience engagement",
     ],
     className: "order-last",
-
-    images: [DM2, DM1, DM3],
+    images: [
+      "/images/services/digital-marketing/dm2.png",
+      "/images/services/digital-marketing/dm1.jpg",
+      "/images/services/digital-marketing/dm3.png",
+    ],
   },
   {
     name: "Apps and Software Development",
     description:
-      "We specialize in developing comprehensive digital solutions, including mobile applications and custom software tailored to your business needs. Our app development services encompass the entire lifecycle, from ideation to deployment, ensuring that we create intuitive and engaging user interfaces through UI/UX design. We build robust native applications for Android and iOS platforms and utilize frameworks like Flutter and React Native for cross-platform development. In software development, we provide bespoke solutions to solve unique business challenges, scalable enterprise applications, and software as a service (SaaS) platform accessible over the internet. Our process includes rigorous testing and quality assurance to guarantee flawless performance and ongoing maintenance and support to keep your solutions updated and running smoothly.",
+      "We specialize in developing comprehensive digital solutions, including mobile applications and custom software...",
     techs: [
       "app development",
       "software development",
@@ -96,12 +83,16 @@ const projects = [
       "scalable solutions",
       "secure software",
     ],
-    images: [App2, App1, App3],
+    images: [
+      "/images/services/app/app2.jpg",
+      "/images/services/app/app1.png",
+      "/images/services/app/app3.png",
+    ],
   },
   {
     name: "Graphics and Motion Designing",
     description:
-      "Our team of creative professionals brings your ideas to life with captivating graphics and animations. We create memorable logos and brand identities, design brochures, flyers, and other promotional materials, and produce engaging visuals tailored for social media platforms. Our expertise in motion graphics and animation allows us to produce dynamic animations for ads, explainer videos, and more, while our infographics transform data into visually appealing and easily understandable formats.",
+      "Our team of creative professionals brings your ideas to life with captivating graphics and animations...",
     techs: [
       "graphics design",
       "motion design",
@@ -113,13 +104,16 @@ const projects = [
       "visual storytelling",
     ],
     className: "order-last",
-
-    images: [GD2, GD3, GD1],
+    images: [
+      "/images/services/graphic/gd3.png",
+      "/images/services/graphic/gd4.png",
+      "/images/services/graphic/gd1.png",
+    ],
   },
   {
     name: "Data Analytics and Visualization",
     description:
-      "We help you harness the power of data to make informed business decisions. Our services include data collection and processing from various sources, implementing business intelligence (BI) tools to analyze data and generate insights, and creating dashboards and visual reports that make data easy to understand. We utilize predictive analytics to forecast future trends and outcomes and offer data strategy consulting to help you develop and implement a data-driven strategy.",
+      "We help you harness the power of data to make informed business decisions...",
     techs: [
       "data analytics",
       "data visualization",
@@ -130,6 +124,10 @@ const projects = [
       "BI tools",
       "actionable insights",
     ],
-    images: [Data1, Data3, Data2],
+    images: [
+      "/images/services/data/data1.png",
+      "/images/services/data/data3.png",
+      "/images/services/data/data2.jpg",
+    ],
   },
 ];
