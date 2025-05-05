01b05d57035a49d548dabe3d45a589928efd8344
tyzrex <sulavbaral58@gmail.com>
2025-05-05 21:35:12 +0545

    pipeline fix with static export

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  trailingSlash: true,
+  /* config options here */
+  images: {
+    unoptimized: true,
+  },
   images: {
     remotePatterns: [
       {
