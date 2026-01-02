cb40e19166412275005ccf6b95c5ccdefdf11c1f
tyzrex <sulavbaral58@gmail.com>
2026-01-02 23:39:26 +0545

    remove workflow

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  trailingSlash: true,
-  /* config options here */
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
